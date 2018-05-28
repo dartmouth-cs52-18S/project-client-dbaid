097dd06696ed178b2eb51cac64a3ffc26037f353
Brian Tomasco <brian.a.tomasco.20@dartmouth.edu>
2018-05-28 15:36:04 -0400

    Loading token from local storage and proceeding to proper screen

--- src/navigation/main_switch_nav.js
+++ src/navigation/main_switch_nav.js
@@ -1,9 +1,11 @@
 import { createSwitchNavigator } from 'react-navigation'
 import AuthNavigator from './auth_stack_nav'
 import MainAppNavigator from './main_tab_nav'
+import AuthLoadingScreen from '../components/screens/authAwait/authAwait'
 
 
 const MainNav = createSwitchNavigator({
+  AuthLoading: AuthLoadingScreen,
   AuthFlow: AuthNavigator,
   AppFlow: MainAppNavigator,
 })
