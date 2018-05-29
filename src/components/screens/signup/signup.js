ddcdf2bd500a2a1b03b7c028399c0fea1aa18f2f
zjohnson5455 <zacharyjohnsonri@gmail.com>
2018-05-29 01:04:48 -0400

    can now choose photo on front end

--- src/components/screens/signup/signup.js
+++ src/components/screens/signup/signup.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { View, Button, TextInput } from 'react-native'
+import { View, Button, TextInput, Image } from 'react-native'
 import { TextField } from 'react-native-material-textfield'
 import { connect } from 'react-redux'
+import { ImagePicker } from 'expo'
 import { bindActionCreators } from 'redux'
 import { signupUser } from '../../../actions/index'
 
@@ -17,11 +18,26 @@ class SignUp extends Component {
       username: '',
       bio: '',
       year: 0,
+      image: null,
+    }
+  }
+  // image picking from https://www.youtube.com/watch?v=d1nw3QdLv8w&index=8&list=PLN3n1USn4xlmqhVdKMurNREwtiUpq-SFy
+  pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    })
+
+    console.log(result.uri)
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri })
     }
   }
 
 
   render() {
+    const { image } = this.state
     return (
       <View>
         <View>
@@ -29,6 +45,12 @@ class SignUp extends Component {
             label="Name"
             onChangeText={username => this.setState({ username })}
           />
+          <Button
+            title="Pick an image from camera roll"
+            onPress={this.pickImage}
+          />
+          {image ? (
+            <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />) : null}
           <TextField
             style={styles.inputField}
             label="Email"
