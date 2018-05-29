9bdf725f3560969548d0ae849481c9618c9c2c40
zjohnson5455 <zacharyjohnsonri@gmail.com>
2018-05-28 22:44:27 -0400

    added year and bio. viewing profile is now complete except picture

--- src/components/screens/userProf/userProf.js
+++ src/components/screens/userProf/userProf.js
@@ -6,8 +6,8 @@ import styles from './styles'
 const UserProf = (props) => {
   console.log(props)
   const author = props.navigation.state.params.params.listing.author.username
-  const bio = props.navigation.state.params.params.bio
-  const year = props.navigation.state.params.params.year
+  const bio = props.navigation.state.params.params.listing.author.bio
+  const year = props.navigation.state.params.params.listing.author.year
   return (
     <View style={styles.root}>
       <View style={styles.profile}>
