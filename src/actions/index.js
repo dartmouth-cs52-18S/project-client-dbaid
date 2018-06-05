7d35d6a7509c9290bfce5c341a542e8132ed3f7f
zjohnson5455 <zacharyjohnsonri@gmail.com>
2018-06-04 23:15:21 -0400

    edit profile

--- src/actions/index.js
+++ src/actions/index.js
@@ -108,6 +108,33 @@ export function signupUser(signup, props) {
   }
 }
 
+export function updateUser(id, user) {
+  return (dispatch) => {
+    axios.put(`${ROOT_URL}/${id}`, { id, bio: user.bio })
+      .then((response) => {
+        console.log(response)
+      }).catch((error) => {
+        console.error(error)
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request)
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message)
+        }
+        console.log(error.config)
+      })
+  }
+}
+
 
 // deletes token from AsyncStorage
 // and deauths
